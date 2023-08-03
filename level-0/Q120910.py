2e845409b1b69101038a1b5a8ba9f439922c334f
sysim <liv660n@gmail.com>
2023-08-04 08:34:59 +0900

    세균 증식

---
+++ level-0/Q120910.py
@@ -0,0 +1,12 @@
+# 세균 증식 https://school.programmers.co.kr/learn/courses/30/lessons/120910
+
+# 나의 풀이
+def solution1(n, t):
+    for i in range:
+        n *= 2
+    return n
+
+
+# 다른 사람의 풀이
+def solution(n, t):
+    return n << t
